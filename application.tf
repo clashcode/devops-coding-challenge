8d91aa18c7f4e66f634e98739f8c35e4ecfe185d
Christian Papauschek <christian@journiapp.com>
2023-03-15 13:31:17 +0100

    application cluster

---
+++ application.tf
@@ -0,0 +1,124 @@
+
+
+resource "aws_ecs_cluster" "cluster" {
+  name = "testapp"
+  setting {
+    name  = "containerInsights"
+    value = "enabled"
+  }
+}
+
+resource "aws_ecs_cluster_capacity_providers" "capacity_providers" {
+  cluster_name = aws_ecs_cluster.cluster.name
+}
+
+resource "aws_security_group" "testapp" {
+  name        = "testapp"
+  description = "Security group for testapp"
+  vpc_id      = "vpc-06e54f0e170d7f275"
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_lb" "testapp" {
+  name               = aws_ecs_cluster.cluster.name
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [ aws_security_group.testapp.id ]
+  subnets            = [ "subnet-068d8181df0250c7a", "subnet-0932f2b361be94828" ]
+}
+
+resource "aws_lb_listener" "testapp" {
+  load_balancer_arn = aws_lb.testapp.arn
+  port              = "80"
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.testapp.arn
+  }
+}
+
+resource "aws_lb_target_group" "testapp" {
+  name     = "testapp"
+  port     = 80
+  protocol = "HTTP"
+  target_type = "ip"
+  vpc_id   = aws_security_group.testapp.vpc_id
+
+  health_check {
+    matcher = "200"
+    path = "/api/v1.0/health/ready"
+    port = 80
+  }
+}
+
+resource "aws_ecs_task_definition" "testapp" {
+  family = "testapp"
+
+  requires_compatibilities = ["FARGATE"]
+  cpu                      = 1024
+  memory                   = 2048
+
+  network_mode       = "awsvpc"
+
+  container_definitions = jsonencode([
+    {
+      name         = "testapp"
+      image        = "yeasy/simple-web:latest"
+      essential    = true
+      portMappings = [
+        {
+          containerPort = 80
+          hostPort      = 80
+        }
+      ]
+      environment = [
+        { name = "HTTPPORT", value = "80" },
+        { name = "MEM_MX", value = "2048m" },
+        { name = "DB_DEFAULT_URL", value = "jdbc:postgresql://testapp.eu-west-1.rds.amazonaws.com:5432/testapp" },
+        { name = "DB_DEFAULT_USER", value = "testapp" },
+        { name = "DB_DEFAULT_PASSWORD", value = "xw3489sf" }
+      ]
+    }
+  ])
+}
+
+resource "aws_ecs_service" "testapp" {
+  name            = "testapp"
+  cluster         = aws_ecs_cluster.cluster.name
+  task_definition = "${aws_ecs_task_definition.testapp.id}:${aws_ecs_task_definition.testapp.revision}"
+
+  health_check_grace_period_seconds = 30
+  desired_count = 1
+
+  load_balancer {
+    target_group_arn = aws_lb_target_group.testapp.arn
+    container_name   = "testapp"
+    container_port   = 80
+  }
+
+  capacity_provider_strategy {
+    capacity_provider = "FARGATE"
+    weight = 1
+  }
+
+  network_configuration {
+    subnets            = aws_lb.testapp.subnets
+    security_groups    = aws_lb.testapp.security_groups
+    assign_public_ip   = true
+  }
+
+}
